7c1c6918b19286b2c5b4efe11bb2a48e460311b6
Shohruh88 <shoh5288899@gmail.com>
2025-04-28 22:12:33 +0500

    switch case

---
+++ claudy/service/switch.php
@@ -0,0 +1,12 @@
+<?php
+$kun = 2;
+switch ($kun) {
+    case 1: echo "Dushanba"; break;
+    case 2: echo "Seshanba"; break;
+    case 3: echo "Chorshanba"; break;
+    case 4: echo "Payshanba"; break;
+    case 5: echo "Juma"; break;
+    case 6: echo "Shanba"; break;
+    case 1: echo "Yakshanba"; break;
+    default: echo "Bunday kun mavjud emas"; break;
+}
